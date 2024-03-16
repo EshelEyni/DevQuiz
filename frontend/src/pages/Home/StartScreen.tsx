6b1c8b1792c51efe94934317b58ed48be31830ce
Eshel Eyni <esheleyni@gmail.com>
2024-03-16 16:46:09 +0200

    added copy btn to all questions, reset questions btn

--- frontend/src/pages/Home/StartScreen.tsx
+++ frontend/src/pages/Home/StartScreen.tsx
@@ -6,10 +6,14 @@ import { AppDispatch } from "../../types/app.types";
 import { Button } from "../../components/Btns/Button";
 import { useState } from "react";
 import classnames from "classnames";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 export const StartScreen = () => {
   const dispatch: AppDispatch = useDispatch();
+  const navigate = useNavigate();
   const { language, numQuestions } = useQuiz();
   const [isFocused, setIsFocused] = useState(false);
+  const { loggedInUser } = useAuth();
 
   useKey("Enter", handleStartQuiz);
   useKey("ArrowDown", handleArrowDown);
@@ -27,12 +31,17 @@ export const StartScreen = () => {
     setIsFocused(true);
   }
 
+  function goToProfilePage() {
+    if (!loggedInUser) return;
+    navigate(`/profile/${loggedInUser.id}`);
+  }
+
   return (
     <div className="flex flex-1 flex-col items-center gap-3 px-3">
       <h2 className="w-full text-center text-4xl font-bold text-gray-50">{`Welcome to The ${language} Quiz!`}</h2>
       <h3 className="w-full text-center text-xl font-bold text-gray-50 ">{`${numQuestions} question to test your ${language} mastery`}</h3>
 
-      {numQuestions > 0 && (
+      {numQuestions > 0 ? (
         <Button
           onClickFn={handleStartQuiz}
           className={classnames(
@@ -44,6 +53,19 @@ export const StartScreen = () => {
         >
           {"Let's start"}
         </Button>
+      ) : (
+        <>
+          <p className="mt-10 text-center text-2xl font-bold text-gray-50">
+            You have completed all the questions. Go to profile to reset the
+            questions.
+          </p>
+          <Button
+            onClickFn={goToProfilePage}
+            className="mt-10 rounded-full bg-gray-600 px-8 py-6 text-3xl font-medium uppercase text-gray-100 transition-all duration-300"
+          >
+            Go to Profile
+          </Button>
+        </>
       )}
     </div>
   );
--- frontend/src/pages/QuestionManagement/QuestionList.tsx
+++ frontend/src/pages/QuestionManagement/QuestionList.tsx
@@ -1,9 +1,15 @@
 import { FC } from "react";
 import { QuestionPreview } from "./QuestionPreview";
-import { getRandomBrightColor } from "../../services/utils.service";
+import {
+  copyToClipboard,
+  getRandomBrightColor,
+} from "../../services/utils.service";
 import { QuestionLoader } from "../../components/Loaders/QuestionLoader/QuestionLoader";
 import { useQuestion } from "../../hooks/useQuestion";
 import { useIntersectionPagination } from "../../hooks/useIntersectionPagination";
+import { Button } from "../../components/Btns/Button";
+import { FaRegCopy } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 type QuestionListProps = {
   noResMsg?: string;
@@ -24,6 +30,25 @@ export const QuestionList: FC<QuestionListProps> = ({
     getQuestionsState.state === "succeeded" && questions.length === 0;
   const isQuestionListShown =
     getQuestionsState.state !== "loading" && questions.length > 0;
+
+  function copyQuestionsToClipboard() {
+    const questionsToCopy = questions
+      .map((q, i) => {
+        return `${i + 1}. ${q.question}\n`;
+      })
+      .join("");
+
+    copyToClipboard(questionsToCopy);
+
+    toast.success("Questions copied to clipboard", {
+      style: {
+        background: "#333",
+        color: "#fff",
+        fontSize: "13px",
+        fontWeight: "600",
+      },
+    });
+  }
 
   return (
     <>
@@ -48,7 +73,12 @@ export const QuestionList: FC<QuestionListProps> = ({
       {isQuestionListShown && (
         <div className="mx-auto mt-4 flex w-11/12 flex-col gap-5 pb-24 md:mt-2">
           <div className="flex w-full flex-col flex-wrap justify-between gap-1 md:flex-row">
-            <p className="text-3xl font-semibold leading-none">{`Number of Questions: ${questions.length}`}</p>
+            <div className="flex items-center gap-2">
+              <p className="text-3xl font-semibold leading-none">{`Number of Questions: ${questions.length}`}</p>
+              <Button onClickFn={copyQuestionsToClipboard}>
+                <FaRegCopy className="text-5xl md:text-4xl" />
+              </Button>
+            </div>
             <p className="text-3xl font-semibold leading-none">{`Number of Approved Questions: ${approvedQuestions.length}`}</p>
             <p className="text-3xl font-semibold leading-none">
               <em className="mr-[4px]">{percentageOfApprovedQuestions}%</em>
